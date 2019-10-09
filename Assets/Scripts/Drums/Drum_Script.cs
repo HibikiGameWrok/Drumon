e52701cd47fb8cae01f27dd438ec97457140f062
FukuyamaRenya <fukuyamarenya2018@gmail.com>
2019-10-09 17:45:39 +0900

    基底クラスのDrumクラスを追加

---
+++ Assets/Scripts/Drums/Drum_Script.cs
@@ -0,0 +1,32 @@
+﻿/*----------------------------------------------------------*/
+//  file:      Drum_Script.cs					                        |
+//				 															    |
+//  brief:    ドラムの基底クラスのスクリプト				    |
+//              Drum base class 				                        |
+//																				|
+//  date:	2019.10.9												    |
+//																				|
+//  author: Renya Fukuyama										|
+/*----------------------------------------------------------*/
+
+// using
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Drumクラスの定義
+public class Drum_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
