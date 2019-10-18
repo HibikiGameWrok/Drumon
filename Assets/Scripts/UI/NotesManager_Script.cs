d4c6eddc3b765c7be196bcf965b1e0e57bf6bb6f
hibiki414 <gameword.hibiki@gmail.com>
2019-10-18 20:33:24 +0900

    子のノーツの名前を取得するScriptを追加

---
+++ Assets/Scripts/UI/NotesManager_Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+
+public class NotesManager_Script : MonoBehaviour
+{
+    // 子のノーツプレハブから数字を保持する変数
+    private string m_childNumSequence;
+
+    public string SearchInstanceNotes()
+    {
+        foreach (Transform child in transform)
+        {
+            m_childNumSequence += Regex.Replace(child.ToString(), @"[^0-9]", "");
+        }
+        return m_childNumSequence;
+    }
+}
