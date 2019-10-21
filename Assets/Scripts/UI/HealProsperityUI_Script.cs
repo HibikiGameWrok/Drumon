0323f43ae59504d7be6f746632ec0edc75d75d64
hibiki414 <gameword.hibiki@gmail.com>
2019-10-21 19:37:49 +0900

    クリーチャーHPUIに反映する処理Scriptを追加

---
+++ Assets/Scripts/UI/HealProsperityUI_Script.cs
@@ -0,0 +1,65 @@
+﻿//
+//      FileName @ HealProspenityUI_Scrip.cs
+//
+//      Creater  @ Hibiki Yoshiyasu
+//
+//      Day      @ 2019 / 10 / 16 (Wednesday)    
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealProsperityUI_Script : MonoBehaviour
+{
+    // 体力用のスライダー
+    private Slider m_hpSlider;
+
+    // スライダー最大値
+    private float m_maxPoint;
+    public float MaxPoint
+    {
+        set { this.m_maxPoint = value; }
+    }
+
+    // スライダー最低値
+    private float m_minPoint;
+    public float MinPoint
+    {
+        set { this.m_minPoint = value; }
+    }
+
+    // 現在の値
+    private float m_nowPoint;
+    public float NowPoint
+    {
+        set { this.m_nowPoint = value; }
+    }
+
+    // アクティブ時に呼ばれる関数
+    void Awake()
+    {
+        m_hpSlider = GetComponent<Slider>();
+    }
+
+    // 更新関数
+    void Update()
+    {
+        SetSliderValue();
+        SetNowValue();
+    }
+
+    // UIに値を設定する関数
+    private void SetSliderValue()
+    {
+        m_hpSlider.maxValue = m_maxPoint;
+        m_hpSlider.maxValue = m_minPoint;
+    }
+
+    // UIの現在の値を設定する関数
+    private void SetNowValue()
+    {
+        m_hpSlider.value = m_nowPoint;
+    }
+
+}
