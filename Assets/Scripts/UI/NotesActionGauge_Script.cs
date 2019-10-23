d70e6f82dda6bae449949d5785808acf4c23f3f9
hibiki414 <gameword.hibiki@gmail.com>
2019-10-23 16:14:23 +0900

    ノーツが出現する処理をNotesInstanceReceive_Scriptに移行

--- Assets/Scripts/UI/NotesActionGauge_Script.cs
+++ Assets/Scripts/UI/NotesActionGauge_Script.cs
@@ -16,54 +16,22 @@ public class NotesActionGauge_Script : MonoBehaviour
     // ゲージの最大値
     public const int MAX_GAUGE = 4;
 
-    // ノーツの生成最大値
-    public const int MAX_INS_NOTES = 8;
-
-    public enum NOTES_TYPE : int
-    {
-        NONE,               // 何もない
-        ONE_IN_HIT,           // １スティックで内側を叩いた時
-        DOUBLE_IN_HIT,        // ２スティックで内側を叩いた時
-        ONE_OUT_HIT,          // １スティックで外側を叩いた時
-        DOUBLE_OUT_HIT,       // ２スティックで外側を叩いた時
-    }
-
-    // 出すノーツの種類
-    NOTES_TYPE m_notesType;
-
     // ノーツの速度
     [SerializeField]
     private float m_notesVel = 0.01f;
 
-    // ノーツの生成数を保持する変数
-    private int m_notesCount = 0;
-
-
-    // ゲージの進行を可視化する為のハンドル
-    private Transform m_childHandle = null;
-    // ノーツをまとめる親オブジェクト
-    private Transform m_notesManager = null;
-    // ノーツをまとめる親オブジェクトにアタッチしているスクリプトを取得
-    private NotesManager_Script m_notesmanager_script = null;
-    // ノーツプレハブ
-    private GameObject m_prefabNotes = null;
-
-
-
     // 行動ゲージが終わったか
     private bool m_finishFlag = false;
 
+    // ゲージの進行を可視化する為のハンドル
+    private Transform m_childHandle = null;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         // 子のオブジェクトを取得
         m_childHandle = this.transform.Find("Handle");
-        m_notesManager = this.transform.Find("NotesManager");
-        // 子のオブジェクトにアタッチしているScriptを取得
-        m_notesmanager_script = m_notesManager.GetComponent<NotesManager_Script>();
-        // プレハブを取得
-        m_prefabNotes = (GameObject)Resources.Load("InsPrefab/NotesPrefab");
     }
 
 
@@ -72,9 +40,6 @@ public class NotesActionGauge_Script : MonoBehaviour
     {
         // ハンドルの動作処理
         HandleMove();
-
-        /// デバッグ用関数 ///
-        KeyNotesinstace();
     }
 
 
@@ -95,84 +60,6 @@ public class NotesActionGauge_Script : MonoBehaviour
                 // 到達したならば動作終了フラグを立てる
                 m_finishFlag = true;
             }
-        }
-    }
-
-
-    // ドラムとスティックが当たると呼ばれる関数
-    // 引数 : NOTES_TYPE type -- 叩き方
-    public void InstantiateNotes(NOTES_TYPE type)
-    {
-        // ノーツのプレハブタイプを設定しロードする
-        SetNotesPrefabPath(type);
-        // ノーツの現在の数が設定値に達成していなければ
-        if (m_notesCount < MAX_INS_NOTES)
-        {
-            // ハンドルの位置にプレハブを生成
-            GameObject InsNotes = (GameObject)Instantiate(m_prefabNotes, m_childHandle.transform.position, Quaternion.identity);
-            InsNotes.transform.parent = m_notesManager.transform;
-            // ノーツの数を増加
-            m_notesCount++;
-        }
-    }
-
-
-    // ドラムの叩き方によって、出すノールのプレハブをロードする関数
-    // InstantiateNotes関数で呼ばれる
-    // 引数 : NOTES_TYPE type -- 叩き方
-    private void SetNotesPrefabPath(NOTES_TYPE type)
-    {
-        // プレハブを取得
-        m_prefabNotes = (GameObject)Resources.Load("InsPrefab/NotesPrefab" + (int)type);
-    }
-
-
-    // ノーツとゲージをリセットする
-    // どこでも呼べるようにpublic化
-    public void NotesReset()
-    {
-        // ノーツの数を０にする
-        m_notesCount = 0;
-        // ノーツ管理オブジェクトの子として生成されたノーツオブジェクトを全て消す
-        foreach (Transform n in m_notesManager.transform)
-        {
-            GameObject.Destroy(n.gameObject);
-        }
-
-        // 子を初期位置に戻す
-        m_childHandle.transform.position = new Vector3(this.transform.position.x - MIN_GAUGE, this.transform.position.y, this.transform.position.z);
-        // 終了フラグを初期化
-        m_finishFlag = false;
-    }
-
-
-    // デバッグ用キーによってノーツを出す
-    private void KeyNotesinstace()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            NotesReset();
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            InstantiateNotes(NOTES_TYPE.ONE_IN_HIT);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            InstantiateNotes(NOTES_TYPE.DOUBLE_IN_HIT);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            InstantiateNotes(NOTES_TYPE.ONE_OUT_HIT);
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            InstantiateNotes(NOTES_TYPE.DOUBLE_OUT_HIT);
-        }
-
-        if (OVRInput.GetDown(OVRInput.RawButton.A))
-        {
-            NotesReset();
         }
     }
 }
