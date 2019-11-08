322bb09421c06ca3e1bc1b5cb1bc80105e39adf6
hibiki414 <gameword.hibiki@gmail.com>
2019-11-08 11:07:04 +0900

    OVRCameraに追従するScriptを追加

---
+++ Assets/Scripts/UI/Test/TestOVRCameraFollowing.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+#pragma warning disable 618
+
+public class TestOVRCameraFollowing : MonoBehaviour
+{
+    private GameObject m_localAvatar = null;
+    private Transform m_oVRCameraRig = null;
+    private Transform m_trackingSpace = null;
+    private Transform m_centerEyeAnchor = null;
+
+    [SerializeField]
+    private float m_spaceY = 0.0f;
+    private float m_distanceY = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (XRDevice.isPresent)
+        {
+            m_localAvatar = GameObject.Find("LocalAvatarWithGrab");
+            m_oVRCameraRig = m_localAvatar.transform.FindChild("OVRCameraRig");
+            m_trackingSpace = m_oVRCameraRig.transform.FindChild("TrackingSpace");
+            m_centerEyeAnchor = m_trackingSpace.transform.FindChild("CenterEyeAnchor");
+
+            m_distanceY = m_spaceY - Mathf.Abs(m_centerEyeAnchor.transform.position.y);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Oculus Riftが接続されているならば
+        if (XRDevice.isPresent)
+        {
+            this.transform.position = new Vector3(this.transform.position.x,m_centerEyeAnchor.transform.position.y - m_distanceY, this.transform.position.z);
+        }
+    }
+}
