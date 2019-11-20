c5ad574e8c4d5f777f7ad705c4b26d1ac3ccf604
YutoIsogai <yutoisogai2018@gmail.com>
2019-11-20 10:35:54 +0900

    シーン間のデータ移行作業中

---
+++ Assets/Scripts/SceneChenge/EnemyHit_Script.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyHit_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //--------------------------
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SceneManager.LoadScene("LoadSceneTest2", LoadSceneMode.Additive);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SceneManager.UnloadSceneAsync("LoadSceneTest2");
+        }
+        //--------------------------
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("LoadSceneTest2", LoadSceneMode.Additive);
+        }
+    }
+}
