f3b7898c7e3e6d1859b3aa4d1120aa77cb5a2f78
FukuyamaRenya <fukuyamarenya2018@gmail.com>
2019-11-21 16:45:04 +0900

    学祭用に

--- Assets/Scripts/VR/TestScript/ButtonAction.cs
+++ Assets/Scripts/VR/TestScript/ButtonAction.cs
@@ -14,7 +14,7 @@ public class ButtonAction : MonoBehaviour
             .Where(_ => OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
             .Subscribe(_ =>
             {
-                TransitionManager_Script.StartTransition("");
+                TransitionManager_Script.StartTransition("TestSearchScene");
             });
        
     }
