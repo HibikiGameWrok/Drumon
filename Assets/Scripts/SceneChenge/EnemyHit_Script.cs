e9ca029128426bb56859f4efbbffd45095f12b10
YutoIsogai <yutoisogai2018@gmail.com>
2019-11-22 15:37:17 +0900

    探索シーンの敵の移動を修正

--- Assets/Scripts/SceneChenge/EnemyHit_Script.cs
+++ Assets/Scripts/SceneChenge/EnemyHit_Script.cs
@@ -3,45 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EnemyHit_Script : MonoBehaviour
-{
-    [SerializeField]
-    private CharactorData m_data = null;
-    [SerializeField]
-    private EnemyCreature m_enemyCreature;
-    private EnemyCreature_Script m_enemyScript;
+//public class EnemyHit_Script : MonoBehaviour
+//{
+//    [SerializeField]
+//    private CharactorData m_data = null;
+//    [SerializeField]
+//    private EnemyCreature m_enemyCreature;
+//    private EnemyCreature_Script m_enemyScript;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+//    // Start is called before the first frame update
+//    void Start()
+//    {
 
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //--------------------------
-        //if (Input.GetKeyDown(KeyCode.A))
-        //{
-        //    SceneManager.LoadScene("LoadSceneTest2", LoadSceneMode.Additive);
-        //}
-        //if (Input.GetKeyDown(KeyCode.B))
-        //{
-        //    SceneManager.UnloadSceneAsync("LoadSceneTest2");
-        //}
-        //--------------------------
-    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        //--------------------------
+//        //if (Input.GetKeyDown(KeyCode.A))
+//        //{
+//        //    SceneManager.LoadScene("LoadSceneTest2", LoadSceneMode.Additive);
+//        //}
+//        //if (Input.GetKeyDown(KeyCode.B))
+//        //{
+//        //    SceneManager.UnloadSceneAsync("LoadSceneTest2");
+//        //}
+//        //--------------------------
+//    }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            //SceneManager.LoadScene("TestBattleScene", LoadSceneMode.Additive);
-            // LoadSceneModeで現在のシーンに展開するか決める
-            TransitionManager_Script.StartTransition("TestBattleScene",LoadSceneMode.Additive);
+//    private void OnCollisionEnter(Collision collision)
+//    {
+//        if (collision.gameObject.tag == "Player")
+//        {
+//            //SceneManager.LoadScene("TestBattleScene", LoadSceneMode.Additive);
+//            // LoadSceneModeで現在のシーンに展開するか決める
+//            TransitionManager_Script.StartTransition("TestBattleScene",LoadSceneMode.Additive);
 
-            m_enemyScript = m_enemyCreature.EnemyCreatureObj.GetComponent<EnemyCreature_Script>();
-            m_enemyScript.SetData(m_data);
-        }
-    }
-}
+//            m_enemyScript = m_enemyCreature.EnemyCreatureObj.GetComponent<EnemyCreature_Script>();
+//            m_enemyScript.SetData(m_data);
+//        }
+//    }
+//}
--- Assets/Scripts/SceneChenge/SearchEnemy_Script.cs
+++ Assets/Scripts/SceneChenge/SearchEnemy_Script.cs
@@ -11,72 +11,30 @@ public class SearchEnemy_Script : MonoBehaviour
     private EnemyCreature m_enemyCreature;
     private EnemyCreature_Script m_enemyScript;
 
-    private CharacterController m_enemyController;
-    //　目的地
-    private Vector3 m_destination;
     //　歩くスピード
-    [SerializeField]
-    private float m_walkSpeed = 1.0f;
-    //　速度
-    private Vector3 m_velocity;
-    //　移動方向
-    private Vector3 m_direction;
-    //　到着フラグ
-    private bool m_arrived;
-    //　スタート位置
-    private Vector3 m_startPosition;
-    //　待ち時間
-    [SerializeField]
-    private float m_waitTime = 5.0f;
-    //　経過時間
-    private float m_elapsedTime;
+    private float m_speed = 1.0f;
+    // 回転速度
+    private float m_rotationSpeed = 1.0f;
+    // 目標位置の範囲
+    private float m_posRange = 10.0f;
+    // 目標位置の位置ベクトル
+    private Vector3 m_targetPos;
+    // 近づいたら目標位置を変える
+    private float m_changeTarget = 5.0f;
+    // 目標位置までの距離
+    private float m_targetDistance;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_enemyController = GetComponent<CharacterController>();
-        Vector2 randDestination = Random.insideUnitCircle * 8;
-        m_startPosition = transform.position;
-        m_destination = transform.position;
-        CreateRandomPosition();
-        m_velocity = Vector3.zero;
-        m_arrived = false;
-        m_elapsedTime = 0.0f;
+        m_targetPos = GetRandomPosition(transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (m_arrived == false)
-        {
-            m_velocity = Vector3.zero;
-            m_direction = (m_destination - transform.position).normalized;
-            transform.LookAt(new Vector3(m_destination.x, transform.position.y, m_destination.z));
-            m_velocity = m_direction * m_walkSpeed;
-
-            m_velocity.y += Physics.gravity.y * Time.deltaTime;
-            m_enemyController.Move(m_velocity * Time.deltaTime);
-
-            //　目的地に到着したかどうかの判定
-            if (Vector3.Distance(transform.position, m_destination) < 0.8f)
-            {
-                m_arrived = true;
-            }
-        }
-        // 到着していたら
-        else
-        {
-            m_elapsedTime += Time.deltaTime;
-
-            //　待ち時間を越えたら次の目的地を設定
-            if (m_elapsedTime > m_waitTime)
-            {
-                CreateRandomPosition();
-                //m_destination = setPosition.GetDestination();
-                m_arrived = false;
-                m_elapsedTime = 0.0f;
-            }
-        }
+        m_targetDistance = Vector3.SqrMagnitude(transform.position - m_targetPos);
+        MoveEnemy();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -91,11 +49,21 @@ public class SearchEnemy_Script : MonoBehaviour
     }
 
     // ランダムな位置の作成
-    private void CreateRandomPosition()
+    Vector3 GetRandomPosition(Vector3 currentpos)
     {
-        //　ランダムなVector2の値を得る
-        var randDestination = Random.insideUnitCircle * 8;
-        //　現在地にランダムな位置を足して目的地とする
-        m_destination = m_startPosition + new Vector3(randDestination.x, 0, randDestination.y);
+        return new Vector3(Random.Range(-m_posRange + currentpos.x, m_posRange + currentpos.x), transform.position.y, Random.Range(-m_posRange + currentpos.z, m_posRange + currentpos.z));
+    }
+
+    // 移動
+    void MoveEnemy()
+    {
+        if (m_targetDistance < m_changeTarget)
+        {
+            m_targetPos = GetRandomPosition(transform.position);
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(m_targetPos - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * m_rotationSpeed);
+        transform.Translate(Vector3.forward * m_speed * Time.deltaTime);
     }
 }
