7b4ac145770f9a6bf4d1ff8d516c123c89965794
hibiki414 <gameword.hibiki@gmail.com>
2019-11-26 19:39:37 +0900

    攻撃レシピ用ノーツ生成Scriptを追加

--- Assets/Scripts/UI/AttackRecipeNotesUI_Script.cs
+++ Assets/Scripts/UI/AttackRecipeNotesUI_Script.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 #pragma warning disable 618
 
-public class AttackRecipeUI_Script : MonoBehaviour
+public class AttackRecipeNotesUI_Script : MonoBehaviour
 {
     // 列のデータタイプ
     public enum Data_Column : int
@@ -18,11 +18,11 @@ public class AttackRecipeUI_Script : MonoBehaviour
         ATK_RATE,
     }
 
-    // Textがアタッチされたオブジェクトの生成数
-    private const int MAX_COUNT_OBJECT = 5 + 1;
-
     // プレハブのファイルパス
     private const string FILEPATH_NOTES = "InsPrefab/RecipeNotes/NotePrefab";
+
+    // Textがアタッチされたオブジェクトの生成数
+    private const int MAX_COUNT_OBJECT = 5 + 1;
 
     // 取得するべき物
     // 前にでているクリーチャーの名前
@@ -34,35 +34,38 @@ public class AttackRecipeUI_Script : MonoBehaviour
     // CSVの中身を入れるリスト;
     private List<string[]> csvDatas = new List<string[]>();
 
-    // 子のオブジェクト
-    private Transform m_canvas = null;
-
     // レシピのノーツを保管する親オブジェクト
     private Transform []m_AbilitySheetObject = new Transform[5];
-
-    // テキストを取得
-    private GameObject []m_textObject = new GameObject[MAX_COUNT_OBJECT];
 
     // ノーツプレハブを保持
     private GameObject m_notesPrefab = null;
 
+    // ノーツをずらす高さ
+    [SerializeField]
+    private float m_notesShiftHeight = 0.0f;
+
+    // ノーツをずらす高さ
+    [SerializeField]
+    private float m_notesShiftWidth = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        LoadCSVFile(BattleManager_Script.Get.PlayerCreature.Name);
+        InstanceRecipeNote();
+        this.transform.localRotation = this.transform.parent.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-
-    public void LoadCSVFile()
+    // CSVを読み込む
+    public void LoadCSVFile(string creatureName)
     {
         // 現在出ているクリチャーの名前を戦闘管理オブジェクトから取得
-        m_nowCreaterName = Regex.Replace(BattleManager_Script.Get.PlayerCreature.Name, @"[^a-z,A-Z]", "");
+        m_nowCreaterName = Regex.Replace(creatureName, @"[^a-z,A-Z]", "");
         // 取得したクリーチャーと同じ名前のCSVファイルを読み込む
         csvFile = Resources.Load("CSV/" + m_nowCreaterName + "CSV") as TextAsset;
         StringReader reader = new StringReader(csvFile.text);
@@ -76,23 +79,26 @@ public class AttackRecipeUI_Script : MonoBehaviour
         }
     }
 
-    private void UIReflect()
+    // ノーツを生成
+    private void InstanceRecipeNote()
     {
         for (int i = 1; i < MAX_COUNT_OBJECT; i++)
         {
             //1文字ずつ列挙する
             for (int j = 0; j < csvDatas[i][(int)Data_Column.ATK_NOTES].Length; j++)
             {
-                Vector3 notePos = new Vector3(m_AbilitySheetObject[i - 1].transform.position.x + 0.05f * j, m_AbilitySheetObject[i - 1].transform.position.y, m_AbilitySheetObject[i - 1].transform.position.z);
-
+                // 技のノーツ数列を１桁取得
                 int stringNotesNum = System.Convert.ToInt32(csvDatas[i][(int)Data_Column.ATK_NOTES].Substring(j, 1));
+
+                // ノーツの座標を設定(１つのノーツにつきX軸にずらす、１つの技名につきY軸をずらす)
+                Vector3 notePos = new Vector3(this.transform.position.x + m_notesShiftWidth * j, this.transform.position.y - m_notesShiftHeight * (i - 1), this.transform.position.z);
 
                 //プレハブ生成
                 m_notesPrefab = Instantiate(
                    SetLodeNotesPrefab(stringNotesNum),
                    notePos,
-                   new Quaternion(0.0f, 0.0f, 0.0f, 0.0f),
-                   m_AbilitySheetObject[i - 1].transform) as GameObject;
+                   Quaternion.identity,
+                   this.transform) as GameObject;
             }
         }
     }
