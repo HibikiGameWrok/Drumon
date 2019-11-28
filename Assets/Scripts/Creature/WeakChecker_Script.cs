a971c9a55dbc3e4ab316ad090f8b8c3855f75355
NaokiAkiyama <aknao19980808@ezweb.ne.jp>
2019-11-28 16:56:42 +0900

    攻撃計算修正

--- Assets/Scripts/Creature/WeakChecker_Script.cs
+++ Assets/Scripts/Creature/WeakChecker_Script.cs
@@ -2,6 +2,12 @@
 {
     static public float WeakCheck(CreatureDataEntity.ELEM attacker, CreatureDataEntity.ELEM defender)
     {
+        int checker = ((int)attacker + 1) % (int)CreatureDataEntity.ELEM.NUM - ((int)defender + 1) % (int)CreatureDataEntity.ELEM.NUM;
+
+        if (checker < 0)
+            return 1.5f;
+        else if (checker > 0)
+            return 0.5f;
         return 1.0f;
     }
 }
