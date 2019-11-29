6574ee0207490360fdcd4d55bd9e554bf8a56c04
FukuyamaRenya <fukuyamarenya2018@gmail.com>
2019-11-29 10:28:41 +0900

    ランタンとキツネのワールド用Prefabを生成する

---
+++ Assets/Scripts/Creature/Explore/WorldCreatureIdle_Script.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCreatureIdle_Script : WorldCreatureState_Script
+{
+
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    public override void Initialize()
+    {
+        
+    }
+
+    /// <summary>
+    /// 実行する
+    /// </summary>
+    /// <returns></returns>
+    public override bool Execute()
+    {
+
+        // 継続する
+        return true;
+    }
+
+
+    /// <summary>
+    /// 終了処理
+    /// </summary>
+    public override void Dispose()
+    {
+       
+    }
+}
