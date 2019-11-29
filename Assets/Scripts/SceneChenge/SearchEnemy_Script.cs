2afd4cd8ae4c13c25e62f722854d0bd95ced9b07
YutoIsogai <yutoisogai2018@gmail.com>
2019-11-29 17:45:01 +0900

    探索シーンでGarfishに当たったときにシーン変更

--- Assets/Scripts/SceneChenge/SearchEnemy_Script.cs
+++ Assets/Scripts/SceneChenge/SearchEnemy_Script.cs
@@ -12,32 +12,33 @@ public class SearchEnemy_Script : MonoBehaviour
     private EnemyCreature_Script m_enemyScript;
 
     //　歩くスピード
-    private float m_speed = 1.0f;
-    // 回転速度
-    private float m_rotationSpeed = 1.0f;
-    // 目標位置の範囲
-    private float m_posRange = 10.0f;
-    // 目標位置の位置ベクトル
-    private Vector3 m_targetPos;
-    // 近づいたら目標位置を変える
-    private float m_changeTarget = 5.0f;
-    // 目標位置までの距離
-    private float m_targetDistance;
+    //private float m_speed = 1.0f;
+    //// 回転速度
+    //private float m_rotationSpeed = 1.0f;
+    //// 目標位置の範囲
+    //private float m_posRange = 10.0f;
+    //// 目標位置の位置ベクトル
+    //private Vector3 m_targetPos;
+    //// 近づいたら目標位置を変える
+    //private float m_changeTarget = 5.0f;
+    //// 目標位置までの距離
+    //private float m_targetDistance;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_targetPos = GetRandomPosition(transform.position);
+        //m_targetPos = GetRandomPosition(transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_targetDistance = Vector3.SqrMagnitude(transform.position - m_targetPos);
-        MoveEnemy();
+        //m_targetDistance = Vector3.SqrMagnitude(transform.position - m_targetPos);
+        //MoveEnemy();
+        int a = 0;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
@@ -57,21 +58,21 @@ public class SearchEnemy_Script : MonoBehaviour
     }
 
     // ランダムな位置の作成
-    Vector3 GetRandomPosition(Vector3 currentpos)
-    {
-        return new Vector3(Random.Range(-m_posRange + currentpos.x, m_posRange + currentpos.x), transform.position.y, Random.Range(-m_posRange + currentpos.z, m_posRange + currentpos.z));
-    }
+    //Vector3 GetRandomPosition(Vector3 currentpos)
+    //{
+    //    return new Vector3(Random.Range(-m_posRange + currentpos.x, m_posRange + currentpos.x), transform.position.y, Random.Range(-m_posRange + currentpos.z, m_posRange + currentpos.z));
+    //}
 
     // 移動
-    void MoveEnemy()
-    {
-        if (m_targetDistance < m_changeTarget)
-        {
-            m_targetPos = GetRandomPosition(transform.position);
-        }
+    //void MoveEnemy()
+    //{
+    //    if (m_targetDistance < m_changeTarget)
+    //    {
+    //        m_targetPos = GetRandomPosition(transform.position);
+    //    }
 
-        Quaternion targetRotation = Quaternion.LookRotation(m_targetPos - transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * m_rotationSpeed);
-        transform.Translate(Vector3.forward * m_speed * Time.deltaTime);
-    }
+    //    Quaternion targetRotation = Quaternion.LookRotation(m_targetPos - transform.position);
+    //    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * m_rotationSpeed);
+    //    transform.Translate(Vector3.forward * m_speed * Time.deltaTime);
+    //}
 }
