3aac5bdcfb8191fb044cfdd939951497fe284fe3
FukuyamaRenya <fukuyamarenya2018@gmail.com>
2019-11-29 10:05:47 +0900

    探索パートのドラモンの状態遷移を作る準備

---
+++ Assets/Scripts/Creature/Explore/WorldCreatureState_Script.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+// using
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 探索でのドラモンの状態遷移クラス
+public abstract class WorldCreatureState_Script : MonoBehaviour
+{
+    /// <summary>
+    /// 初期化処理
+    /// </summary>
+    public abstract void Initialize();
+
+
+    /// <summary>
+    /// 実行する
+    /// </summary>
+    /// <returns>true=継続する false=状態を変更する </returns>
+    public abstract bool Execute();
+
+
+    /// <summary>
+    /// 終了処理
+    /// </summary>
+    public abstract void Dispose();
+
+}
