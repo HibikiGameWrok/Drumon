045aaeb25b4a0d36c8d5f4f1d885896a46a153d5
NaokiAkiyama <aknao19980808@ezweb.ne.jp>
2019-12-04 17:46:22 +0900

    list start

--- Assets/Scripts/Creature/CreatureList_Script.cs
+++ Assets/Scripts/Creature/CreatureList_Script.cs
@@ -7,6 +7,18 @@ public class CreatureList_Script : SingletonBase_Script<CreatureList_Script>
     [SerializeField]
     private PlayerBox_Script m_list = null;
 
+    void Start()
+    {
+        for (int i = 0; i < m_list.DataList.Length; i++)
+        {
+            if (m_list.DataList[i])
+            {
+                return;
+            }
+        }
+        m_list.DataList[0] = CreateData_Script.Get.CreateData("Merlion");
+    }
+
     public PlayerBox_Script List
     {
         get { return m_list; }
