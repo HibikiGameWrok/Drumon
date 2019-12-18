cb61c4011d0d1590ddb258dfbd60253a2a3f1057
FukuyamaRenya <fukuyamarenya2018@gmail.com>
2019-12-18 11:46:28 +0900

    Battleのposセッティング

--- Assets/Scripts/Player/PlayerPositionGetter.cs
+++ Assets/Scripts/Player/PlayerPositionGetter.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class PlayerPositionGetter : SingletonBase_Script<PlayerPositionGetter>
 {
-
+    [SerializeField]
+    private PlayerPosition_Script m_pos;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        this.transform.position = m_pos.Position;
     }
 }
