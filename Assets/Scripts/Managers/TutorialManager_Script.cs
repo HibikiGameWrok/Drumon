62ea9f01b7e49ab2b711b5bb2cda21bfc92a6976
YutoIsogai <yutoisogai2018@gmail.com>
2020-01-08 17:40:28 +0900

    TutorialManagerの修正

--- Assets/Scripts/Managers/TutorialManager_Script.cs
+++ Assets/Scripts/Managers/TutorialManager_Script.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TutorialManager_Script : SingletonBase_Script<TutorialManager_Script>
 {
@@ -139,46 +140,49 @@ public class TutorialManager_Script : SingletonBase_Script<TutorialManager_Scrip
         m_timer = GameObject.Find("Timer");
         m_timeStandard_Script = m_timer.GetComponent<AccelerationTime_Script>();
 
-        m_tutorialCanvas = GameObject.Find("TutorialCanvas");
-        m_explainBattleSequenceText = m_tutorialCanvas.transform.Find("ExplainBattleSequenceText").gameObject;
-        m_explainDrumonText = m_tutorialCanvas.transform.Find("ExplainDrumonText").gameObject;
-        m_explainHPText = m_tutorialCanvas.transform.Find("ExplainHPText").gameObject;
-        m_explainAttackDrumText = m_tutorialCanvas.transform.Find("ExplainAttackDrumText").gameObject;
-        m_rightArrowText = m_tutorialCanvas.transform.Find("RightArrowText").gameObject;
-        m_explainBattleSystemText = m_tutorialCanvas.transform.Find("ExplainBattleSystemText").gameObject;
-        m_explainBattleSystemText2 = m_tutorialCanvas.transform.Find("ExplainBattleSystemText2").gameObject;
-        m_succesAttackText = m_tutorialCanvas.transform.Find("SuccesAttackText").gameObject;
-        m_rightArrowText3 = m_tutorialCanvas.transform.Find("RightArrowText3").gameObject;
-        m_tutorialEndText = m_tutorialCanvas.transform.Find("TutorialEndText").gameObject;
+        if (SceneManager.GetActiveScene().name != "TutorialCaptureScene")
+        {
+            m_tutorialCanvas = GameObject.Find("TutorialCanvas");
+            m_explainBattleSequenceText = m_tutorialCanvas.transform.Find("ExplainBattleSequenceText").gameObject;
+            m_explainDrumonText = m_tutorialCanvas.transform.Find("ExplainDrumonText").gameObject;
+            m_explainHPText = m_tutorialCanvas.transform.Find("ExplainHPText").gameObject;
+            m_explainAttackDrumText = m_tutorialCanvas.transform.Find("ExplainAttackDrumText").gameObject;
+            m_rightArrowText = m_tutorialCanvas.transform.Find("RightArrowText").gameObject;
+            m_explainBattleSystemText = m_tutorialCanvas.transform.Find("ExplainBattleSystemText").gameObject;
+            m_explainBattleSystemText2 = m_tutorialCanvas.transform.Find("ExplainBattleSystemText2").gameObject;
+            m_succesAttackText = m_tutorialCanvas.transform.Find("SuccesAttackText").gameObject;
+            m_rightArrowText3 = m_tutorialCanvas.transform.Find("RightArrowText3").gameObject;
+            m_tutorialEndText = m_tutorialCanvas.transform.Find("TutorialEndText").gameObject;
 
-        m_inHitCheckImage = m_explainBattleSystemText2.transform.Find("InHitCheckImage").gameObject;
-        m_outHitCheckImage = m_explainBattleSystemText2.transform.Find("OutHitCheckImage").gameObject;
-        m_doubleInHitCheckImage = m_explainBattleSystemText2.transform.Find("DoubleInHitCheckImage").gameObject;
-        m_doubleOutHitCheckImage = m_explainBattleSystemText2.transform.Find("DoubleOutHitCheckImage").gameObject;
+            m_inHitCheckImage = m_explainBattleSystemText2.transform.Find("InHitCheckImage").gameObject;
+            m_outHitCheckImage = m_explainBattleSystemText2.transform.Find("OutHitCheckImage").gameObject;
+            m_doubleInHitCheckImage = m_explainBattleSystemText2.transform.Find("DoubleInHitCheckImage").gameObject;
+            m_doubleOutHitCheckImage = m_explainBattleSystemText2.transform.Find("DoubleOutHitCheckImage").gameObject;
 
-        m_tutorialSwitchCanvas = GameObject.Find("TutorialSwitchCanvas");
-        m_explainSwitchDrumText = m_tutorialSwitchCanvas.transform.Find("ExplainSwitchDrumText").gameObject;
-        m_leftArrowText = m_tutorialSwitchCanvas.transform.Find("LeftArrowText").gameObject;
-        m_practiceChangeText = m_tutorialSwitchCanvas.transform.Find("PracticeChangeText").gameObject;
-        m_succesSwitchText = m_tutorialSwitchCanvas.transform.Find("SuccesSwitchText").gameObject;
-        m_leftArrowText2 = m_tutorialSwitchCanvas.transform.Find("LeftArrowText2").gameObject;
+            m_tutorialSwitchCanvas = GameObject.Find("TutorialSwitchCanvas");
+            m_explainSwitchDrumText = m_tutorialSwitchCanvas.transform.Find("ExplainSwitchDrumText").gameObject;
+            m_leftArrowText = m_tutorialSwitchCanvas.transform.Find("LeftArrowText").gameObject;
+            m_practiceChangeText = m_tutorialSwitchCanvas.transform.Find("PracticeChangeText").gameObject;
+            m_succesSwitchText = m_tutorialSwitchCanvas.transform.Find("SuccesSwitchText").gameObject;
+            m_leftArrowText2 = m_tutorialSwitchCanvas.transform.Find("LeftArrowText2").gameObject;
 
-        m_tutorialCaptureCanvas = GameObject.Find("TutorialCaptureCanvas");
-        m_explainCaptureDrumText = m_tutorialCaptureCanvas.transform.Find("ExplainCaptureDrumText").gameObject;
-        m_rightArrowText2 = m_tutorialCaptureCanvas.transform.Find("RightArrowText2").gameObject;
-        m_practiceCaptureText = m_tutorialCaptureCanvas.transform.Find("PracticeCaptureText").gameObject;
+            m_tutorialCaptureCanvas = GameObject.Find("TutorialCaptureCanvas");
+            m_explainCaptureDrumText = m_tutorialCaptureCanvas.transform.Find("ExplainCaptureDrumText").gameObject;
+            m_rightArrowText2 = m_tutorialCaptureCanvas.transform.Find("RightArrowText2").gameObject;
+            m_practiceCaptureText = m_tutorialCaptureCanvas.transform.Find("PracticeCaptureText").gameObject;
 
-        m_tutorialAbilityCanvas = GameObject.Find("TutorialAbilityCanvas");
-        m_explainAbilityText = m_tutorialAbilityCanvas.transform.Find("ExplainAbilityText").gameObject;
-        m_explainAbilityText2 = m_tutorialAbilityCanvas.transform.Find("ExplainAbilityText2").gameObject;
+            m_tutorialAbilityCanvas = GameObject.Find("TutorialAbilityCanvas");
+            m_explainAbilityText = m_tutorialAbilityCanvas.transform.Find("ExplainAbilityText").gameObject;
+            m_explainAbilityText2 = m_tutorialAbilityCanvas.transform.Find("ExplainAbilityText2").gameObject;
 
-        m_tutorialMusicScoreCanvas = GameObject.Find("TutorialMusicScoreCanvas");
-        m_explainTimerText = m_tutorialMusicScoreCanvas.transform.Find("ExplainTimerText").gameObject;
-        m_explainNotesResetText = m_tutorialMusicScoreCanvas.transform.Find("ExplainNotesResetText").gameObject;
+            m_tutorialMusicScoreCanvas = GameObject.Find("TutorialMusicScoreCanvas");
+            m_explainTimerText = m_tutorialMusicScoreCanvas.transform.Find("ExplainTimerText").gameObject;
+            m_explainNotesResetText = m_tutorialMusicScoreCanvas.transform.Find("ExplainNotesResetText").gameObject;
 
-        m_textArray = new GameObject[] { m_explainBattleSequenceText, m_explainDrumonText, m_explainHPText, m_explainAttackDrumText, m_rightArrowText, m_explainSwitchDrumText, m_leftArrowText, m_explainCaptureDrumText, m_rightArrowText2, m_explainBattleSystemText, m_explainBattleSystemText2, m_explainAbilityText, m_explainAbilityText2, m_explainTimerText, m_explainNotesResetText, m_succesAttackText, m_rightArrowText3, m_practiceChangeText, m_succesSwitchText, m_leftArrowText2, m_practiceCaptureText, m_tutorialEndText };
+            m_textArray = new GameObject[] { m_explainBattleSequenceText, m_explainDrumonText, m_explainHPText, m_explainAttackDrumText, m_rightArrowText, m_explainSwitchDrumText, m_leftArrowText, m_explainCaptureDrumText, m_rightArrowText2, m_explainBattleSystemText, m_explainBattleSystemText2, m_explainAbilityText, m_explainAbilityText2, m_explainTimerText, m_explainNotesResetText, m_succesAttackText, m_rightArrowText3, m_practiceChangeText, m_succesSwitchText, m_leftArrowText2, m_practiceCaptureText, m_tutorialEndText };
 
-        m_text = m_textArray[0].GetComponent<Text>();
+            m_text = m_textArray[0].GetComponent<Text>();
+        }
 
         m_drumManager = GameObject.Find("DrumManager").GetComponent<DrumManager_Script>();
     }
@@ -190,8 +194,11 @@ public class TutorialManager_Script : SingletonBase_Script<TutorialManager_Scrip
         {
             this.m_attackSpan -= Time.deltaTime;
 
-            if (this.JudgeResult()) return;
-            this.m_playerCreature.Execute();
+            if (SceneManager.GetActiveScene().name != "TutorialCaptureScene")
+            {
+                if (this.JudgeResult()) return;
+                this.m_playerCreature.Execute();
+            }
 
             if (m_enemyExecuteFlag == false)
             {
@@ -208,118 +215,122 @@ public class TutorialManager_Script : SingletonBase_Script<TutorialManager_Scrip
         //{
         //    m_timeStandard_Script.StopFlag = true;
         //}
-        if (m_tutorialModeFlag == false)
-        {
-            // 文字を小さくする
-            m_text.fontSize -= 10;
-        }
 
-        // ボタンが押されたら
-        if (OVRInput.GetDown(OVRInput.RawButton.A) && m_practiceModeFlag == false)
+        if (SceneManager.GetActiveScene().name != "TutorialCaptureScene")
         {
-            m_tutorialModeFlag = false;
-        }
-
-        // 文字が消えたら
-        if (m_text.fontSize <= 0)
-        {
-            // 次のテキストの表示
-            NextText();
-        }
-
-        if (m_curentNum == 10)
-        {
-            m_practiceModeFlag = true;
-        }
-        else if (m_curentNum == 14)
-        {
-            m_practiceModeFlag = true;
-            m_timeStandard_Script.StopFlag = false;
-        }
-        else if (m_curentNum == 17)
-        {
-            m_practiceModeFlag = true;
-        }
-
-        if (m_practiceCaptureText.activeInHierarchy == true)
-        {
-            m_practiceModeFlag = true;
-            //m_timeStandard_Script.StopFlag = false;
-            m_drumManager.CaptureDrum.GetComponent<CaptureDrum_Script>().TutorialCaptureFlag = true;
-        }
-
-        // 叩けたらチェックを出す
-        if (m_explainBattleSystemText2.activeInHierarchy == true)
-        {
-            if (m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.IsFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.IN_HIT) == true || Input.GetKeyDown(KeyCode.V))
+            if (m_tutorialModeFlag == false)
             {
-                m_inHitCheckImage.SetActive(true);
+                // 文字を小さくする
+                m_text.fontSize -= 10;
             }
-            if (m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.IsFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.OUT_HIT) == true || Input.GetKeyDown(KeyCode.B))
-            {
-                m_outHitCheckImage.SetActive(true);
-            }
-            if (m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.IsFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.DOUBLE_IN_HIT) == true || Input.GetKeyDown(KeyCode.N))
-            {
-                m_doubleInHitCheckImage.SetActive(true);
-            }
-            if (m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.IsFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.DOUBLE_OUT_HIT) == true || Input.GetKeyDown(KeyCode.M))
-            {
-                m_doubleOutHitCheckImage.SetActive(true);
-            }
-            // 全てのチェックが出たら
-            if (m_inHitCheckImage.activeInHierarchy == true && m_outHitCheckImage.activeInHierarchy == true && m_doubleInHitCheckImage.activeInHierarchy == true && m_doubleOutHitCheckImage.activeInHierarchy == true)
+
+            // ボタンが押されたら
+            if (OVRInput.GetDown(OVRInput.RawButton.A) && m_practiceModeFlag == false)
             {
                 m_tutorialModeFlag = false;
             }
-        }
-        else
-        {
-            m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.OffFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.IN_HIT);
-            m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.OffFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.OUT_HIT);
-            m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.OffFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.DOUBLE_IN_HIT);
-            m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.OffFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.DOUBLE_OUT_HIT);
-        }
 
-        if (m_explainNotesResetText.activeInHierarchy == true)
-        {
-            m_enemyExecuteFlag = false;
-
-            // 敵が攻撃を受けたら
-            if (m_enemyCreature.HP < m_enemyCreature.GetData().maxHp)
-            {
-                //if (m_timeStandard_Script.StopFlag == false)
-                //{
-                //    m_timeStandard_Script.StopFlag = true;
-
-                //    // 次のテキストの表示
-                //    NextText();
-                //}
-
-                // 次のテキストの表示
-                NextText();
-            }
-        }
-
-        if (m_practiceChangeText.activeInHierarchy == true)
-        {
-            // モンスターを変更したら
-            if (m_drumManager.SwitchDrum.GetComponent<SwitchDrum_Script>().TutorialChengeFlag == true)
+            // 文字が消えたら
+            if (m_text.fontSize <= 0)
             {
                 // 次のテキストの表示
                 NextText();
-
-                m_drumManager.SwitchDrum.GetComponent<SwitchDrum_Script>().TutorialChengeFlag = false;
             }
-        }
 
-        if (m_drumManager.TutorialGetFlag == true)
-        {
-            // 次のテキストの表示
-            NextText();
+            if (m_curentNum == 10)
+            {
+                m_practiceModeFlag = true;
+            }
+            else if (m_curentNum == 14)
+            {
+                m_practiceModeFlag = true;
+                m_timeStandard_Script.StopFlag = false;
+            }
+            else if (m_curentNum == 17)
+            {
+                m_practiceModeFlag = true;
+            }
 
-            m_drumManager.TutorialGetFlag = false;
-            //m_timeStandard_Script.StopFlag = true;
+            if (m_practiceCaptureText.activeInHierarchy == true)
+            {
+                m_practiceModeFlag = true;
+                //m_timeStandard_Script.StopFlag = false;
+                m_drumManager.CaptureDrum.GetComponent<CaptureDrum_Script>().TutorialCaptureFlag = true;
+            }
+
+            // 叩けたらチェックを出す
+            if (m_explainBattleSystemText2.activeInHierarchy == true)
+            {
+                if (m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.IsFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.IN_HIT) == true || Input.GetKeyDown(KeyCode.V))
+                {
+                    m_inHitCheckImage.SetActive(true);
+                }
+                if (m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.IsFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.OUT_HIT) == true || Input.GetKeyDown(KeyCode.B))
+                {
+                    m_outHitCheckImage.SetActive(true);
+                }
+                if (m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.IsFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.DOUBLE_IN_HIT) == true || Input.GetKeyDown(KeyCode.N))
+                {
+                    m_doubleInHitCheckImage.SetActive(true);
+                }
+                if (m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.IsFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.DOUBLE_OUT_HIT) == true || Input.GetKeyDown(KeyCode.M))
+                {
+                    m_doubleOutHitCheckImage.SetActive(true);
+                }
+                // 全てのチェックが出たら
+                if (m_inHitCheckImage.activeInHierarchy == true && m_outHitCheckImage.activeInHierarchy == true && m_doubleInHitCheckImage.activeInHierarchy == true && m_doubleOutHitCheckImage.activeInHierarchy == true)
+                {
+                    m_tutorialModeFlag = false;
+                }
+            }
+            else
+            {
+                m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.OffFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.IN_HIT);
+                m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.OffFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.OUT_HIT);
+                m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.OffFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.DOUBLE_IN_HIT);
+                m_drumManager.AttackDrum.GetComponent<AttackDrum_Script>().TutorialFlag.OffFlag((uint)AttackDrum_Script.TUTORIAL_HIT_PATTERN.DOUBLE_OUT_HIT);
+            }
+
+            if (m_explainNotesResetText.activeInHierarchy == true)
+            {
+                m_enemyExecuteFlag = false;
+
+                // 敵が攻撃を受けたら
+                if (m_enemyCreature.HP < m_enemyCreature.GetData().maxHp)
+                {
+                    //if (m_timeStandard_Script.StopFlag == false)
+                    //{
+                    //    m_timeStandard_Script.StopFlag = true;
+
+                    //    // 次のテキストの表示
+                    //    NextText();
+                    //}
+
+                    // 次のテキストの表示
+                    NextText();
+                }
+            }
+
+            if (m_practiceChangeText.activeInHierarchy == true)
+            {
+                // モンスターを変更したら
+                if (m_drumManager.SwitchDrum.GetComponent<SwitchDrum_Script>().TutorialChengeFlag == true)
+                {
+                    // 次のテキストの表示
+                    NextText();
+
+                    m_drumManager.SwitchDrum.GetComponent<SwitchDrum_Script>().TutorialChengeFlag = false;
+                }
+            }
+
+            if (m_drumManager.TutorialGetFlag == true)
+            {
+                // 次のテキストの表示
+                NextText();
+
+                m_drumManager.TutorialGetFlag = false;
+                //m_timeStandard_Script.StopFlag = true;
+            }
         }
     }
 
