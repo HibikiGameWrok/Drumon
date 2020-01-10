df9721408f13b20f9fbf9778f31f90a8eec4024a
hibiki414 <gameword.hibiki@gmail.com>
2020-01-10 20:20:25 +0900

    endingイベント処理

--- Assets/Scripts/UI/EndingEvent_Script.cs
+++ Assets/Scripts/UI/EndingEvent_Script.cs
@@ -5,22 +5,21 @@ using UnityEngine.UI;
 
 public class EndingEvent_Script : MonoBehaviour
 {
-    private GameObject m_canvasObject = null;
+    private string m_textCSVName = "StaffList";
 
-    private string m_textCSVName = "StaffRollText";
+    private CSVDataHolder m_csvHolder = new CSVDataHolder();
+
+    // 設定したCSVの中身を変数に保持
+    List<string[]> m_csvDatas = null;
 
 
-    private CSVDataHolder csvHolder = new CSVDataHolder();
 
     // Start is called before the first frame update
     void Start()
     {
-        if (GameObject.Find("StaffRollCanvas") != null)
-        {
-            m_canvasObject = GameObject.Find("StaffRollCanvas");
-        }
-
-        //CSVSetting(m_textCSVName);
+        CSVSetting(m_textCSVName);
+        m_csvDatas = m_csvHolder.CSVDatas;
+        StartCoroutine("EventProgress");
     }
 
     // Update is called once per frame
@@ -33,13 +32,32 @@ public class EndingEvent_Script : MonoBehaviour
     public void CSVSetting(string textCSVName)
     {
         // CSVの保管クラスに設定
-        csvHolder.CSVLoadFile(textCSVName);
+        m_csvHolder.CSVLoadFile(textCSVName);
     }
 
     private IEnumerator EventProgress()
     {
+        for(int i = 1; i < m_csvDatas.Count; i++)
+        {
+            yield return new WaitForSeconds(2.0f);
+            // CubeプレハブをGameObject型で取得
+            GameObject obj = (GameObject)Resources.Load("InsPrefab/StaffRollTextObject");
+
+            Text stafftext = obj.transform.GetChild(0).GetComponent<Text>();
+            stafftext.text = m_csvDatas[i][1];
+            if(m_csvDatas[i][2] == "1")
+            {
+                stafftext.color = new Color(0.2f,0.4f,0.2f,1); 
+            }
+            else
+            {
+                stafftext.color = Color.black;
+            }
+
+            // Cubeプレハブを元に、インスタンスを生成
+            Instantiate(obj, this.transform.position - new Vector3(0.0f, 2.0f,0.0f), Quaternion.identity,this.transform);
 
 
-        yield break;
+        }
     }
 }
