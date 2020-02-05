95da8194a8db96e517b293c4241c046b8bb98269
hibiki414 <gameword.hibiki@gmail.com>
2020-02-05 16:24:10 +0900

    視覚調整UIを追加

---
+++ Assets/Scripts/UI/VisualAdjustMentUI_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualAdjustMentUI_Script : MonoBehaviour
+{
+    private bool m_switchFlag = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (m_switchFlag == true)
+            {
+                this.GetComponent<SetChildActiveObject_Script>().OpenUI();
+                m_switchFlag = false;
+            }
+            else 
+            {
+                this.GetComponent<SetChildActiveObject_Script>().CloseUI();
+                m_switchFlag = true;
+            }
+        }
+    }
+}
